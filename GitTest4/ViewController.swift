bfad07b0eb4beb1c0c97a78fdc3ec4307d55486d
David Liu <davidliu@Davids-MacBook-Pro.local>
2015-06-17 18:24:27 +0900

    Added first bit of text

--- GitTest4/ViewController.swift
+++ GitTest4/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Added the first bit of code.")
     }
 
     override func didReceiveMemoryWarning() {
